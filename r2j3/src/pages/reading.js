29667953af822e7aa14c9d35a2da11adf389ff2f
jjmonte20 <jjmonte20@gmail.com>
2019-08-24 12:01:27 -0500

    added react-bootstrap

---
+++ r2j3/src/pages/reading.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Reading extends Component {
+    
+    render(){
+        return (
+            "hello world"
+        )
+    }
+}
+
+export default Reading;
