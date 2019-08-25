8fcd8e289c3048fb08a509a223e82fb597faf619
ravithej <ravithej@buffalo.edu>
2019-08-24 23:37:47 -0500

    updated with getMetadataRoutes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 const yControllerRoutes = require("./yControllerRoutes");
 const bookContentRoutes = require("./bookContentRoutes");
 const bookStatusRoutes = require("./bookStatusRoutes");
+const getMetadataRoutes = require("./getMetadataRoute");
 
 
 // User routes
 router.use("/store", yControllerRoutes);
 router.use("/getbookcontent", bookContentRoutes);
 router.use("/bookstatus", bookStatusRoutes)
+router.use("/getMetadata", getMetadataRoutes)
 
 module.exports = router;
