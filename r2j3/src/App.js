a921a267706756d78f5540115b46e0ef17aca37e
jjmonte20 <jjmonte20@gmail.com>
2019-08-24 22:32:20 -0500

    stuff

--- r2j3/src/App.js
+++ r2j3/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/navbar/navbar";
 import Homepage from "./pages/homepage/homepage";
 import Reading from "./pages/reading/reading";
+import "./App.css";
 
 class App extends Component {
 
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={Homepage} />
             <Route exact path="/reading/xdarkmaster512x" component={Reading} />
           </Switch>
+          <p className="disclaimer">Yuuvis slaved all day in the kitchen for this</p>
         </div>
       </Router>
     );
